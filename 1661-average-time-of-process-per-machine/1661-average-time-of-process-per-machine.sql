edcc2ec4b2469d37760a4ffb3516c69841309916
Suyog Repal <suyog.repal23@spit.ac.in>
2026-04-01 00:51:40 +0530

    Time: 232 ms (74.48%), Space: 0B (100%) - LeetHub

---
+++ 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -0,0 +1,7 @@
+select a1.machine_id, round(avg(a2.timestamp - a1.timestamp), 3) as processing_time
+from Activity a1
+left join Activity a2
+on a1.process_id = a2.process_id and a1.machine_id = a2.machine_id
+where a2.activity_type = 'end' and a1.activity_type = 'start'
+group by a1.machine_id
+order by a1.machine_id;
