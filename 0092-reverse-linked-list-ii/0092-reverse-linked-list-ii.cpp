7aa06746e7aba75b72dd61cdfa5b48b7b4ed1ef6
Suyog Repal <suyog.repal23@spit.ac.in>
2026-04-07 01:18:28 +0530

    Time: 0 ms (100%), Space: 11.2 MB (38.31%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
@@ -15,16 +15,17 @@ public:
         ListNode dummy(0); 
         dummy.next = head; 
         ListNode* prev = &dummy; 
+
+        //prev to actual prev before left; 
         for(int i =1; i<left; i++){
             prev = prev->next; 
         }
         ListNode* curr = prev->next; 
-        // reverse the sublist till left-right+1; 
-        for(int i =0; i<right-left; i++){
-           ListNode* temp = curr->next; 
-           curr->next =  temp->next; 
-           temp->next = prev->next; 
-           prev->next = temp; 
+        for(int i = 0; i<right - left; i++){
+            ListNode*  temp = curr->next; 
+            curr->next = temp->next; 
+            temp->next = prev->next; 
+            prev->next = temp; 
         }
         return dummy.next; 
     }
