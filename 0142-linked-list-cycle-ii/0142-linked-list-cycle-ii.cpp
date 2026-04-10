a11a7c2d708bfea800ea4fd44dcc9d689e402e06
Suyog Repal <suyog.repal23@spit.ac.in>
2026-04-10 19:48:04 +0530

    Time: 11 ms (15.03%), Space: 11.4 MB (53.64%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -16,7 +16,7 @@ public:
         fast = fast->next->next; 
           if(slow == fast){
             slow = head; 
-            while(slow != head){
+            while(slow != fast){
                 slow = slow->next; 
                 fast = fast->next; 
             }
