c27fb16b24d893bc5f4aeac7b2d662b1028c636a
Suyog Repal <suyog.repal23@spit.ac.in>
2026-04-11 17:38:23 +0530

    Time: 45 ms (49.43%), Space: 86.8 MB (18.09%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
+        int distinct = 0; 
+        int l = 0; 
+        int maxi  = 0; 
+        
         int n = fruits.size(); 
         unordered_map<int, int> freq; 
-        int ans = 0; 
-        int l = 0; 
-        int distinct = 0; 
         for(int r = 0; r<n; r++){
-            if(freq[fruits[r]] == 0) distinct++; 
-            freq[fruits[r]]++; 
-            while(distinct > 2){
-                freq[fruits[l]]--; 
-                if(freq[fruits[l]] == 0) distinct--; 
-                l++; 
-            }
-            ans = max(ans, r-l+1); 
+           if(freq[fruits[r]] == 0) distinct++; 
+           freq[fruits[r]]++; 
+           while(distinct > 2){
+            freq[fruits[l]]--; 
+            if(freq[fruits[l]]== 0) distinct--; 
+            l++;
+           }
+           maxi = max(maxi, r-l+1); 
         }
-        return ans; 
+        return maxi; 
     }
 };
