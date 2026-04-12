afda084e5d0510076a151324028d5f948b687f0e
Suyog Repal <suyog.repal23@spit.ac.in>
2026-04-12 11:30:46 +0530

    Time: 0 ms (100%), Space: 27.3 MB (51.54%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+ void solve(vector<char>&s, int l, int r){
+            if(l>= r) return;
+            swap(s[l], s[r]); 
+            solve(s, l+1, r-1); 
+        }
+    void reverseString(vector<char>& s) {
+        int n = s.size();
+        solve(s, 0, n-1); 
+    }
+};
