1127799601677d4156f273355be1a9362f80ba52
Suyog Repal <suyog.repal23@spit.ac.in>
2026-04-14 11:43:28 +0530

    Time: 0 ms (100%), Space: 16.3 MB (39.33%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {
-         int m = grid.size(); 
+        int m = grid.size(); 
         int n = grid[0].size(); 
-        vector<vector<int>> dp(m, vector<int>(n));  
-        dp[0][0] =grid[0][0]; 
-        // first row 
-        for(int i = 1; i<n; i++){
-            dp[0][i] = dp[0][i-1] + grid[0][i];  
+        vector<vector<int>> dp(m, vector<int>(n)); 
+        dp[0][0] = grid[0][0]; 
+        for(int j = 1; j<n; j++){
+            dp[0][j] = dp[0][j-1] + grid[0][j]; 
         }
-        // first coloumn; 
-         for(int j = 1; j<m; j++){
-            dp[j][0] = dp[j-1][0] + grid[j][0];  
+       for(int i=1; i<m; i++){
+        dp[i][0] = dp[i-1][0] + grid[i][0]; 
+       }
+       for(int i=1; i<m; i++){
+        for(int j = 1; j<n; j++){
+            dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + grid[i][j]; 
         }
-        for(int i = 1; i<m; i++){
-            for(int j = 1; j<n; j++){
-                dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + grid[i][j];
-            }
-        }
-        return dp[m-1][n-1];
-
+       }
+       return dp[m-1][n-1]; 
     }
 };
