f81febfd2fe65d24546b2d75b67432a54378b4c1
Suyog Repal <suyog.repal23@spit.ac.in>
2026-04-17 19:38:57 +0530

    Time: 7 ms (61%), Space: 11.4 MB (81.26%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,27 +1,29 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        int n = s.size(); 
         int count = t.size(); 
+        int n = s.size(); 
+        if(s.size() < t.size()) return ""; 
         int minLength = INT_MAX; 
-        unordered_map<char, int> freq; 
+        unordered_map<char, int> freq;
         for(char c: t) freq[c]++; 
-        int start = 0; 
         int l = 0; 
-        for(int r = 0; r<n; r++){
-            if(freq[s[r]] >0) count--; 
-            freq[s[r]]--; 
-            while(count ==0){
+        int start = 0; 
+        for(int r =0; r<n; r++){
+             if(freq[s[r]]>0) count--; 
+             freq[s[r]]--; 
+             while(count == 0){
                 if(r-l+1 < minLength){
-                  minLength = r-l+1;
-                  start = l; 
+                   minLength = r-l+1; 
+                   start = l; 
                 }
                 freq[s[l]]++; 
-                if(freq[s[l]]>0) count++; 
-                l++;  
-            }
-        }
+                if(freq[s[l]] >0) count++; 
+                l++; 
+             }
+        } 
         if(minLength == INT_MAX) return ""; 
-        return s.substr(start, minLength); 
+        return s.substr(start, minLength);
+
     }
 };
