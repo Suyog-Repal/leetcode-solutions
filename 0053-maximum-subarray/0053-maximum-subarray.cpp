33b02612dbaea8a148b31bba484a48b3addc1365
Suyog Repal <suyog.repal23@spit.ac.in>
2026-04-19 00:09:05 +0530

    Time: 0 ms (100%), Space: 71.7 MB (79.98%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
+        int n = nums.size(); 
+        int sum = 0; 
         int maxi = INT_MIN; 
-        int sum = 0;
-        int n = nums.size();  
         for(int i =0; i<n; i++){
             sum += nums[i]; 
             maxi = max(maxi, sum); 
-            if(sum < 0) sum = 0; 
+            if(sum < 0) sum = 0;  
         }
         return maxi; 
     }
