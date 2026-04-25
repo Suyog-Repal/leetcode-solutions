1090e66c7c1da3034001020236f6b2f122a4b845
Suyog Repal <suyog.repal23@spit.ac.in>
2026-04-26 01:31:47 +0530

    Time: 0 ms (100%), Space: 17.8 MB (9.97%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -6,8 +6,9 @@ public:
         dp[0] = 0; 
         dp[1] = 0; 
         for(int i =2; i<=n; i++){
-            dp[i] = min(dp[i-1] + cost[i-1] , dp[i-2] + cost[i-2]); 
+            dp[i] = min(dp[i-1] + cost[i-1], dp[i-2]+cost[i-2]);
         }
-        return dp[n]; 
+        return dp[n];
+         
     }
 };
