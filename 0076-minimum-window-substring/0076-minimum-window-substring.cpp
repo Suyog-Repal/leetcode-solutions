a6b1431dc885491206a1ba5b39ece192708e5d05
Suyog Repal <suyog.repal23@spit.ac.in>
2026-05-12 21:09:46 +0530

    Time: 14 ms (37.48%), Space: 11.5 MB (62.01%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        int count = t.size(); 
-        int n = s.size(); 
         if(s.size() < t.size()) return ""; 
-        int minLength = INT_MAX; 
-        unordered_map<char, int> freq;
+        unordered_map<char, int> freq; 
+        int count = t.size(); 
+        int n = s.size();
         for(char c: t) freq[c]++; 
-        int l = 0; 
         int start = 0; 
-        for(int r =0; r<n; r++){
-             if(freq[s[r]]>0) count--; 
-             freq[s[r]]--; 
-             while(count == 0){
-                if(r-l+1 < minLength){
-                   minLength = r-l+1; 
-                   start = l; 
+        int minLen = 1e7; 
+        int l = 0; 
+        for(int r = 0; r<n; r++){
+            freq[s[r]]--; 
+            if(freq[s[r]] >=0) count--; 
+            while(count == 0){
+                if(r-l+1 < minLen){
+                    minLen = r-l+1;
+                    start = l; 
                 }
                 freq[s[l]]++; 
-                if(freq[s[l]] >0) count++; 
+                if(freq[s[l]]>0) count++; 
                 l++; 
-             }
-        } 
-        if(minLength == INT_MAX) return ""; 
-        return s.substr(start, minLength);
-
+            }
+        }
+        if(minLen == 1e7) return ""; 
+        return s.substr(start, minLen); 
     }
 };
