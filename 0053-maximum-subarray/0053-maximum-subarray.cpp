5c982029e1cdcac7609e71681240c3fe705b34e0
Suyog Repal <suyog.repal23@spit.ac.in>
2026-05-14 22:38:47 +0530

    Time: 0 ms (100%), Space: 71.7 MB (79.84%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -6,7 +6,7 @@ public:
         for(int i =0; i<nums.size(); i++){
             sum += nums[i]; 
             maxi = max(maxi, sum); 
-            if(sum < 0) sum = 0; 
+          if(sum < 0) sum= 0; 
         }
         return maxi; 
     }
