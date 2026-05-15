4d58ca45e7eb1caa4799137071577bf314a6deee
Suyog Repal <suyog.repal23@spit.ac.in>
2026-05-15 13:15:09 +0530

    Time: 0 ms (100%), Space: 11.1 MB (42.75%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -11,15 +11,15 @@
  */
 class Solution {
 public:
-    void postorder(TreeNode* root, vector<int> & result){
+    void helper(TreeNode* root, vector<int> & result){
         if(root == NULL) return; 
-        postorder(root->left, result); 
-        postorder(root->right, result); 
+        helper(root->left, result); 
+        helper(root->right, result); 
         result.push_back(root->val); 
     }
     vector<int> postorderTraversal(TreeNode* root) {
         vector<int> result; 
-        postorder(root, result); 
+        helper(root, result); 
         return result; 
     }
 };
