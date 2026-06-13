dab70d147d97e9a6f3228e659d418dd5556bb00c
Suyog Repal <suyog.repal23@spit.ac.in>
2026-06-13 21:25:37 +0530

    Time: 0 ms (100%), Space: 8.1 MB (17.48%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
     int fib(int n) {
+        if(n == 0) return 0; 
+        if(n == 1) return 1; 
         vector<int> dp(n+1); 
-        if(n<=1) return n; 
         dp[0] = 0; 
         dp[1] = 1; 
         for(int i =2; i<=n; i++){
