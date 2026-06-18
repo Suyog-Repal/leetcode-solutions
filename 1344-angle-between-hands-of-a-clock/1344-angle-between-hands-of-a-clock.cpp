66aae3bdf485712de8ed22b0a029546c20c05442
Suyog Repal <suyog.repal23@spit.ac.in>
2026-06-18 19:57:16 +0530

    Time: 0 ms (100%), Space: 8.2 MB (46.25%) - LeetHub

---
+++ 1344-angle-between-hands-of-a-clock/1344-angle-between-hands-of-a-clock.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    double angleClock(int hour, int minutes) {
+        double ans = 0; 
+        ans = abs(30*hour - 5.5*minutes); 
+        if(ans > 180){
+            ans = 360 - ans; 
+        }
+        return ans; 
+    }
+};
