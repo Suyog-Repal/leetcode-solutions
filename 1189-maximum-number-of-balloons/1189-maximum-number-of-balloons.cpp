f5b334b1d846c0e9ec7e8f57d930490f2e19ac0d
Suyog Repal <suyog.repal23@spit.ac.in>
2026-06-22 14:59:56 +0530

    Time: 3 ms (12.33%), Space: 8.9 MB (84.03%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
@@ -1,10 +1,8 @@
 class Solution {
 public:
     int maxNumberOfBalloons(string text) {
-        
-            int b = 0, a = 0, l = 0, o = 0 ,n = 0;
+        int b = 0, a = 0, l = 0, o = 0 ,n = 0;
         for(char c : text){
-
             if(c == 'b'){
                 b++;
             }else if(c == 'a'){
