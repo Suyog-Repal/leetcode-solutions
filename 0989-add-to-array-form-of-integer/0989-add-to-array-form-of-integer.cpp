6f8a70f8f6df6c74dcb23ff87f064f32cb121095
Suyog Repal <suyog.repal23@spit.ac.in>
2026-07-12 00:58:28 +0530

    Time: 0 ms (100%), Space: 32.9 MB (5.04%) - LeetHub

--- 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.cpp
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.cpp
@@ -2,28 +2,17 @@ class Solution {
 public:
     vector<int> addToArrayForm(vector<int>& num, int k) {
         vector<int> result; 
-        int n = num.size(); 
-        string ans = to_string(k); 
-        int carry = 0;
-        for(int i = n-1; i>=0; i--){
-            int digit = 0;
-           if(!ans.empty()){
-               digit = ans.back() - '0';
-               ans.pop_back();
-           } 
-            int sum = num[i] + digit + carry; 
-           result.push_back(sum%10);
+        int carry = 0; 
+        int i = num.size()-1;
+        while(i>=0 || k>0 || carry > 0){
+            int digit = k%10; 
+            int sum = digit + carry; 
+            if(i>=0) sum += num[i--];
+            result.push_back(sum%10); 
             carry = sum/10; 
+            k = k/10; 
         }
-        while(!ans.empty()){
-             int digit = ans.back() - '0'; 
-             ans.pop_back(); 
-             int sum = digit + carry; 
-             result.push_back(sum%10); 
-             carry = sum/10;
-        }
-        if(carry > 0) result.push_back(carry);
-        reverse(result.begin(), result.end());
-            return result;
+        reverse(result.begin(), result.end()); 
+        return result; 
     }
 };
