2737a02874b0efae93eca5c8b9f089f70fbb707f
Suyog Repal <suyog.repal23@spit.ac.in>
2026-07-15 12:48:34 +0530

    Time: 3 ms (16.66%), Space: 8.5 MB (85.02%) - LeetHub

---
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+        int odd = 0; 
+        int even = 0; 
+        for(int i = 1; i<=2*n; i++){
+            if(i%2 == 0){
+                even += i; 
+            }else{
+                odd+=i;
+            }
+        }
+      int a = even, b = odd; 
+      while(a != 0 && b != 0){
+          if(a > b) a = a%b; 
+          else b = b%a; 
+
+      }
+      if(a ==0) return b; 
+     return a; 
+    }
+};
