a341f55dcfc9ee699214c5aa4c115b689ded5cde
Suyog Repal <suyog.repal23@spit.ac.in>
2026-07-16 16:22:47 +0530

    Time: 90 ms (10.29%), Space: 155.5 MB (95.24%) - LeetHub

--- 3867-sum-of-gcd-of-formed-pairs/3867-sum-of-gcd-of-formed-pairs.cpp
+++ 3867-sum-of-gcd-of-formed-pairs/3867-sum-of-gcd-of-formed-pairs.cpp
@@ -1,23 +1,23 @@
-class  Solution {
-    public: 
-     long long gcdSum(vector<int>& nums){
-      int n = nums.size(); 
-      if(n == 0) return 0; 
-        vector<long long> prefix(n); 
-        int  maxi = nums[0]; 
+class Solution {
+public:
+    long long gcdSum(vector<int>& nums) {
+        int n = nums.size(); 
+        vector<int> prefix(n); 
         prefix[0] = nums[0]; 
-        for(int  i = 1; i<n; i++){
-            maxi = max(maxi, nums[i]); 
-            prefix[i] = gcd(maxi, nums[i]); 
+        int maxi = nums[0]; 
+        
+        for(int i = 1; i<n; i++){
+          maxi = max(maxi, nums[i]); 
+          prefix[i] = gcd(maxi, nums[i]); 
         }
         sort(prefix.begin(), prefix.end()); 
-        long long left = 0; 
-        long long  right = n-1; 
+        int left = 0; 
+        int right = n-1; 
         long long sum = 0; 
-        while(left<right){
-           sum += gcd(prefix[left], prefix[right]);
+        while(left < right){
+           sum += gcd(prefix[left], prefix[right]); 
            left++; 
-           right--;  
+           right--;
         }
         return sum; 
     }
