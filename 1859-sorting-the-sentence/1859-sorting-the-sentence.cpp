56806ee9ba3ba1d9d94cb150eddef9382a9fc686
Suyog Repal <suyog.repal23@spit.ac.in>
2026-07-21 19:20:34 +0530

    Time: 0 ms (100%), Space: 8.4 MB (70.13%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.cpp
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.cpp
@@ -1,28 +1,19 @@
 class Solution {
 public:
     string sortSentence(string s) {
-        vector<string> temp; 
-        int start = 0; 
-        int n = s.size(); 
-        for(int i = 0; i<n; i++){
-            if(s[i] == ' '){
-             temp.push_back(s.substr(start, i-start));
-             start = i+1;
-            }
-        }
-        temp.push_back(s.substr(start, n-start)); 
+        stringstream ss(s); 
+        string word; 
         map<int, string> mp; 
-        
-        for(string t: temp){
-           int x = t.back(); 
-           t.pop_back(); 
-           mp[x] = t; 
+        while(ss >> word){
+            int x = word.back(); 
+            word.pop_back(); 
+            mp[x] = word; 
         }
-        string ans = ""; 
-        for(auto&p: mp){
-            ans+=p.second + ' ';
+        string ans = "";
+        for(auto &p: mp){
+           ans+=p.second+' ';
         }
-        ans.pop_back();
+        ans.pop_back(); 
         return ans; 
     }
 };
