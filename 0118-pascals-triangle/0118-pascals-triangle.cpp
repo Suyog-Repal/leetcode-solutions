54e2e4346456134b1453b75030e263fc9a362b63
Suyog Repal <suyog.repal23@spit.ac.in>
2026-07-21 16:14:57 +0530

    Time: 0 ms (100%), Space: 9.7 MB (30.52%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -3,13 +3,11 @@ public:
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> result; 
         for(int i = 0; i<numRows; i++){
-            vector<int> temp; 
-            long long coeff = 1; 
-            for(int j = 0; j<=i; j++){
-                temp.push_back(coeff); 
-                coeff = coeff*(i-j)/(j+1);
+            vector<int> rows(i+1, 1);
+            for(int j = 1; j<i; j++){
+                rows[j] = result[i-1][j-1] + result[i-1][j]; 
             }
-            result.push_back(temp); 
+            result.push_back(rows); 
         }
         return result; 
     }
