cd2d075fb04c05b8a0be59332ec17ee500625ec2
Suyog Repal <suyog.repal23@spit.ac.in>
2026-07-22 01:04:41 +0530

    Time: 0 ms (100%), Space: 26.7 MB (42.52%) - LeetHub

---
+++ 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isPalindrome(string s){
+        int n = s.size(); 
+        for(int i = 0;i<n; i++){
+            if(s[i] != s[n-i-1]){
+                return false; 
+            }
+        }
+        return true; 
+    }
+    string firstPalindrome(vector<string>& words) {
+        int n = words.size(); 
+        for(int i =0;i<n;i++){
+            if(isPalindrome(words[i])){
+                return words[i]; 
+            }
+        }
+        return ""; 
+        
+    }
+};
