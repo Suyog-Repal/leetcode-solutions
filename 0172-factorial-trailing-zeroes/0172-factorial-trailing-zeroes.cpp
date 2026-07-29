3ba5c03b022336908d72592092dfe60a21d797e7
Suyog Repal <suyog.repal23@spit.ac.in>
2026-07-29 08:16:20 +0530

    Time: 0 ms (100%), Space: 8.1 MB (87.52%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
@@ -4,15 +4,14 @@ public:
      int count2 = 0; 
      int x = n, y = n; 
      while(x>0){
-        x = x/2; 
-        count2+=x; 
+       x = x/2; 
+       count2+=x;
      }
      int count5=0;
      while(y>0){
-        y =y/5; 
+        y = y/5; 
         count5+=y;
      }
-     return min(count2, count5); 
-     
+     return min(count2, count5);
     }
 };
