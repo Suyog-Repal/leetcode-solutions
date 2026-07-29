a61e477e2223493fe283209408e5e4a84e372f19
Suyog Repal <suyog.repal23@spit.ac.in>
2026-07-29 23:44:06 +0530

    Time: 0 ms (100%), Space: 45.3 MB (69.6%) - LeetHub

--- 3512-minimum-operations-to-make-array-sum-divisible-by-k/3512-minimum-operations-to-make-array-sum-divisible-by-k.cpp
+++ 3512-minimum-operations-to-make-array-sum-divisible-by-k/3512-minimum-operations-to-make-array-sum-divisible-by-k.cpp
@@ -3,7 +3,6 @@ public:
     int minOperations(vector<int>& nums, int k) {
       int sum = 0;
       for(int x: nums) sum+=x;
-      if(sum%k==0) return 0;
-      return sum%k; 
+      return sum%k;
     }
 };
