80836d09158c890e2c17a3d2ed387d0eb2fac7b4
Suyog Repal <suyog.repal23@spit.ac.in>
2026-07-29 08:14:43 +0530

    Time: 2 ms (8.85%), Space: 8.1 MB (87.52%) - LeetHub

---
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int trailingZeroes(int n) {
+     int count2 = 0; 
+     int x = n, y = n; 
+     while(x>0){
+        x = x/2; 
+        count2+=x; 
+     }
+     int count5=0;
+     while(y>0){
+        y =y/5; 
+        count5+=y;
+     }
+     return min(count2, count5); 
+     
+    }
+};
