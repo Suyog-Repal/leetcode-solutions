ecb84abd622b99b7ec4cb9d1f3eda4f8bca80125
Suyog Repal <suyog.repal23@spit.ac.in>
2026-08-04 00:10:27 +0530

    Time: 3 ms (57.43%), Space: 10.4 MB (16.29%) - LeetHub

---
+++ 0819-most-common-word/0819-most-common-word.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    string mostCommonWord(string paragraph, vector<string>& banned) {
+        unordered_map<string, int> freq; 
+        string para = "";
+        for(char c: paragraph){
+            if(ispunct(c)){
+                para += ' ';
+            }else
+            para+= tolower(c);
+        }
+        cout << para << endl; 
+        stringstream ss(para); 
+        string word; 
+        while(ss >> word){
+             
+           freq[word]++; 
+           for(string s: banned){
+            if(word == s) freq[word] = 0;
+           }
+        }
+        int maxi = 0;
+        string ans; 
+        for(auto &p: freq){
+             if(p.second > maxi){
+                 maxi = p.second;
+                 ans = p.first;
+            }
+        }
+        
+        return ans;
+    }
+};
