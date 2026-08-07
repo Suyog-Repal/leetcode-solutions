a792c63f9990dfd3317bd7b51add348d0fbbd25f
Suyog Repal <suyog.repal23@spit.ac.in>
2026-08-07 19:32:16 +0530

    Time: 4 ms (25.13%), Space: 11.5 MB (49.43%) - LeetHub

--- 1408-string-matching-in-an-array/1408-string-matching-in-an-array.cpp
+++ 1408-string-matching-in-an-array/1408-string-matching-in-an-array.cpp
@@ -5,13 +5,13 @@ public:
         int n = words.size();
        for(int i=0;i<n;i++){
          for(int j = 0;j<n; j++){
-           if(i!=j && words[i].find(words[j]) != string::npos){
-             result.push_back(words[j]); 
+           if(i!=j && words[j].find(words[i]) != string::npos){
+             result.push_back(words[i]); 
+             break; 
            }
          }
        }
-       sort(result.begin(), result.end()); 
-       result.erase(unique(result.begin(), result.end()), result.end());
+
        return result; 
     }
 };
