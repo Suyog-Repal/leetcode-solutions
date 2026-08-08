f06e27e8bab34be6dd55d023213bcabdd9695609
Suyog Repal <suyog.repal23@spit.ac.in>
2026-08-08 22:26:23 +0530

    Time: 0 ms (100%), Space: 8.3 MB (73.78%) - LeetHub

---
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int countGoodSubstrings(string s) {
+        int k = 0; 
+        int n = s.size();
+        int ans =0;
+        for(int i=0;i<n-2;i++){
+             bool equal = false;
+            if(s[i]==s[i+1] || s[i]==s[i+2] || s[i+1] == s[i+2]){
+                    equal = true;
+             }
+            if(!equal) ans++; 
+        }
+        return ans; 
+    }
+};
