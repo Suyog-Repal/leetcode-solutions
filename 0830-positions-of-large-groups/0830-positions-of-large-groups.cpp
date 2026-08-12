28a53bb1677cd3d9ab6fb16856ce3ec7f538dc25
Suyog Repal <suyog.repal23@spit.ac.in>
2026-08-12 16:18:18 +0530

    Time: 7 ms (1.21%), Space: 10.6 MB (12.84%) - LeetHub

---
+++ 0830-positions-of-large-groups/0830-positions-of-large-groups.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>> largeGroupPositions(string s) {
+        int n = s.size(); 
+        vector<vector<int>> result; 
+        for(int i=0;i<n;i++){
+            vector<int> temp; 
+            int count =0;
+            for(int j = i;j<n;j++){
+                if(s[i] == s[j]){
+                   count++;
+                }else break; 
+            }
+           // cout << count << endl;
+            if(count>=3){
+                cout << i << endl;
+              temp.push_back(i);
+              temp.push_back(i+count-1);
+              i +=count-1;
+            }else continue;
+            result.push_back(temp);
+        }
+        return result; 
+    }
+};
