d46c549b60df546e15f13b824fe13cdede697511
Suyog Repal <suyog.repal23@spit.ac.in>
2026-08-14 11:48:07 +0530

    Time: 2 ms (15.69%), Space: 23.7 MB (83.27%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int l= 0; 
-        int r = 0; 
+        int j = 0; 
         int n = nums.size();
-        while(l<n){
-            if(nums[l] !=0){
-                swap(nums[l], nums[r]);
-                r++; 
+        for(int i=0;i<n;i++){
+            if(nums[i]!=0){
+                swap(nums[i], nums[j]); 
+                j++;
             }
-            l++; 
         }
     }
 };
