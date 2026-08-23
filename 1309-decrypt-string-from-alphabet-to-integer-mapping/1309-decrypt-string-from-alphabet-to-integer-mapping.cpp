17d92425fcc0ba989e256fe848c7b37fbfa7a8f9
Suyog Repal <suyog.repal23@spit.ac.in>
2026-08-23 15:10:36 +0530

    Time: 3 ms (6.54%), Space: 9 MB (15.19%) - LeetHub

--- 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.cpp
+++ 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.cpp
@@ -2,19 +2,19 @@ class Solution {
 public:
     string freqAlphabets(string s) {
         unordered_map<string, char> mp; 
-        int n = s.size();
         for(int i=0;i<26;i++){
-          mp[to_string(i+1)] = 'a'+i;
+          mp[to_string(i+1)] = 'a'+i; 
         }
-        string ans ="";
+        string ans = "";
+        int n = s.size();
         for(int i=0;i<s.size(); i++){
-            string c; 
-            if(i+2<n && s[i+2] == '#'){
-                c = s.substr(i, 2);
-                ans+= mp[c];
-                i +=2; 
-            }else ans += mp[string(1, s[i])];
+            if(i+2 < n && s[i+2] == '#'){
+               ans+= mp[s.substr(i, 2)]; 
+               i+=2; 
+            }else{
+                ans+=mp[string(1, s[i])]; 
+            }
         }
-       return ans; 
-    }
+        return ans; 
+    } 
 };
