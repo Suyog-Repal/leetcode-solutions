0f77dbf241adeeefdb71e197c064f396c21ae624
Suyog Repal <suyog.repal23@spit.ac.in>
2026-08-25 00:51:37 +0530

    Time: 39 ms (8.67%), Space: 8.3 MB (53.84%) - LeetHub

---
+++ 4000-largest-integer-with-given-digit-sum/4000-largest-integer-with-given-digit-sum.cpp
@@ -0,0 +1,24 @@
+class Solution{
+public:
+    int digitsum(int n){
+        int sum = 0; 
+        while(n){
+            int digit = n%10;
+            sum+=digit; 
+            n=n/10;
+        }
+        return sum;
+    }
+    int largestInteger(int n, int s) {
+      int x = pow(10, n); 
+      
+      int maxi = -1;
+       for(int i=x-1; i>=0;i--){
+          if(digitsum(i) == s){
+            maxi = max(maxi, i);
+            break; 
+          }
+       }
+       return maxi; 
+    }
+};
