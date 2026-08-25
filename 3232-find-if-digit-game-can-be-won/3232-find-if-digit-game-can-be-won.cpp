bf4569459eba3270075bd9da4f3282573c7bb95f
Suyog Repal <suyog.repal23@spit.ac.in>
2026-08-26 01:52:59 +0530

    Time: 4 ms (2.24%), Space: 28.7 MB (49.3%) - LeetHub

---
+++ 3232-find-if-digit-game-can-be-won/3232-find-if-digit-game-can-be-won.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool canAliceWin(vector<int>& nums) {
+        int singlesum= 0, doublesum = 0;
+        int n= nums.size(); 
+        for(int x: nums){
+            if(x<10) singlesum += x;
+            else doublesum+=x;
+        }
+        if(singlesum == doublesum) return false; 
+        return true;
+    }
+};
