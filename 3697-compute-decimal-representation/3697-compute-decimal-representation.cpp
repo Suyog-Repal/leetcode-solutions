e9ff89a69ca7e4cd8d252e21ca9c5624827c0811
Suyog Repal <suyog.repal23@spit.ac.in>
2026-08-28 00:56:00 +0530

    Time: 0 ms (100%), Space: 9.5 MB (76.43%) - LeetHub

---
+++ 3697-compute-decimal-representation/3697-compute-decimal-representation.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> decimalRepresentation(int n) {
+        vector<int>result; 
+        long long count = 1; 
+        while(n){
+            int digit = n%10; 
+            if(count*digit !=0){
+                result.push_back(count*digit);
+            }
+            count*=10;
+            n=n/10;
+        }
+        reverse(result.begin(), result.end()); 
+
+        return result; 
+    }
+};
