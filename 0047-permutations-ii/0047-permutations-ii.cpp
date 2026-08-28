4bfdc843993189e4981a4d766374f79d7204b0af
Suyog Repal <suyog.repal23@spit.ac.in>
2026-08-28 17:53:54 +0530

    Time: 12 ms (18.18%), Space: 15.3 MB (12.76%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-   void  generatepermutation(vector<int> &nums, int index, vector<vector<int>> &result){
+    void generatepermutation(vector<int>&nums, int index, vector<vector<int>>&result){
         if(index == nums.size()){
             result.push_back(nums); 
-            return; 
+            return;
         }
         unordered_set<int>used; 
-        for(int i = index;i<nums.size(); i++){
-            if(used.count(nums[i])) continue;
-            used.insert(nums[i]);
+        for(int i=index;i<nums.size(); i++){
+            if(used.count(nums[i])) continue; 
+            used.insert(nums[i]); 
             swap(nums[index], nums[i]); 
             generatepermutation(nums, index+1, result); 
-            swap(nums[i], nums[index]);
+            swap(nums[i], nums[index]); 
         }
     }
     vector<vector<int>> permuteUnique(vector<int>& nums) {
