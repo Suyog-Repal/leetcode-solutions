936aea91142d79c07043e9df3080b9765497d5ad
Suyog Repal <suyog.repal23@spit.ac.in>
2026-08-30 12:53:57 +0530

    Time: 3 ms (5.38%), Space: 8.7 MB (17.99%) - LeetHub

---
+++ 3908-valid-digit-number/3908-valid-digit-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool validDigit(int n, int x) {
+        string s = to_string(n); 
+        if(s[0]-'0' == x) return false; 
+        cout << s[0]-'0' << endl;
+        char p = x+'0';
+        cout << p << endl;
+        int count = 0;
+        for(char c: s){
+            if(c ==p){
+                count++; 
+            }
+        }
+        if(s[0]-'0' != x && count >= 1) return true;
+        return false;
+    }
+};
