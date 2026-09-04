0976634b38d205869154dc06600406e6e3de558e
Suyog Repal <suyog.repal23@spit.ac.in>
2026-09-05 00:02:13 +0530

    Time: 2 ms (29.25%), Space: 31.2 MB (56.73%) - LeetHub

---
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        int n = nums.size(); 
+        vector<int> instable(n); 
+        int ans = -1; 
+        for(int i=0; i<n;i++){
+            int x = *max_element(nums.begin(), nums.begin()+i); 
+            int y = *min_element(nums.begin()+i, nums.end()); 
+            if(x- y <=k){
+                ans = i; 
+                break;
+            }
+        }
+       return ans; 
+    }
+};
