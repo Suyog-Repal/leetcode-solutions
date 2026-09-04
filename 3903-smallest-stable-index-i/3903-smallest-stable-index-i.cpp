1c0c11d977eda2955753a55e75e7df4b580d3598
Suyog Repal <suyog.repal23@spit.ac.in>
2026-09-05 00:06:38 +0530

    Time: 0 ms (100%), Space: 30.3 MB (76.6%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     int firstStableIndex(vector<int>& nums, int k) {
-        int n = nums.size(); 
-        vector<int> instable(n); 
+        int n = nums.size();  
         int ans = -1; 
         for(int i=0; i<n;i++){
             int x = *max_element(nums.begin(), nums.begin()+i); 
