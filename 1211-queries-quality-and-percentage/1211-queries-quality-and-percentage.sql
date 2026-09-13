78294c3fb10b6612f1ca00a55c907be58c6d99eb
Suyog Repal <suyog.repal23@spit.ac.in>
2026-09-13 18:46:04 +0530

    Time: 529 ms (11.39%), Space: 0B (100%) - LeetHub

---
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -0,0 +1,3 @@
+select query_name, round(avg(rating/position), 2) as quality, round(count(case when rating <3 then 1 end)*100/count(*), 2) as poor_query_percentage
+from Queries q
+group by query_name;
