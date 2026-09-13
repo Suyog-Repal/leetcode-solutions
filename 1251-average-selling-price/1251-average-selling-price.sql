8a125900bdf2de782b43ee3a7c2eecd2f38f58c5
Suyog Repal <suyog.repal23@spit.ac.in>
2026-09-13 17:34:34 +0530

    Time: 824 ms (80.6%), Space: 0B (100%) - LeetHub

--- 1251-average-selling-price/1251-average-selling-price.sql
+++ 1251-average-selling-price/1251-average-selling-price.sql
@@ -1,10 +1,10 @@
 with cte as(
-select p.product_id,  start_date, end_date, price, u.purchase_date, units
-from Prices p
-left join UnitsSold u
-on p.product_id = u.product_id AND purchase_date between start_date and end_date
+    select p.product_id, p.start_date, p.end_date, p.price, u.purchase_date, u.units
+    from Prices p
+    left join UnitsSold u
+    on p.product_id = u.product_id and purchase_date between start_date and end_date
 )
-select product_id, ifnull(round(sum(price*units)/sum(units), 2), 0) as average_price
+
+select product_id, round(ifnull(sum(price*units)/(sum(units)), 0), 2) as average_price
 from cte
 group by product_id;
-
