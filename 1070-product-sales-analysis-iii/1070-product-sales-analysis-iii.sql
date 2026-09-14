849eeba564ade69d9b65d99c029324ede33f24da
Suyog Repal <suyog.repal23@spit.ac.in>
2026-09-14 15:45:10 +0530

    Time: 907 ms (19.5%), Space: 0B (100%) - LeetHub

---
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -0,0 +1,8 @@
+with cte as (
+    select product_id, year,  min(year) over (partition by product_id) as first_year,
+  quantity, price
+from Sales
+)
+select product_id, first_year, quantity, price
+from cte 
+where year = first_year;
