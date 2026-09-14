a8784c6d0f43de0d1df67804b8497cd6dc89207c
Suyog Repal <suyog.repal23@spit.ac.in>
2026-09-14 15:53:51 +0530

    Time: 701 ms (85.86%), Space: 0B (100%) - LeetHub

--- 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -1,9 +1,8 @@
 with cte as (
-    select distinct product_id, min(year) over (partition by product_id) as first_year
+    select product_id, year, min(year) over (partition by product_id) as first_year,
+     quantity,  price
     from Sales
 )
-select s.product_id, 
-c.first_year, s.quantity, s.price
-from Sales s
-join cte c
-on s.product_id = c.product_id and s.year = c.first_year;
+select product_id, year as first_year, quantity, price
+from cte 
+where year = first_year;
