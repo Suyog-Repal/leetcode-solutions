dc6be26b2ca8eb2f583da36e9505870bbc8639d5
Suyog Repal <suyog.repal23@spit.ac.in>
2026-09-16 00:33:31 +0530

    Time: 1537 ms (73.63%), Space: 0B (100%) - LeetHub

---
+++ 1907-count-salary-categories/1907-count-salary-categories.sql
@@ -0,0 +1,5 @@
+select 'Low Salary' as Category, sum(if(income < 20000, 1, 0)) as accounts_count from Accounts
+union
+select 'Average Salary' as category, sum(if(income between 20000 and 50000 , 1, 0)) as accounts_count from Accounts
+union 
+select 'High Salary' as Category, sum(if(income > 50000, 1, 0)) as accounts_count  from Accounts; 
