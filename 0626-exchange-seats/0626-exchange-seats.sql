3e69a5766d4a8a81c3b8a40a278eb8729de60d3b
Suyog Repal <suyog.repal23@spit.ac.in>
2026-09-17 16:14:06 +0530

    Time: 404 ms (40.14%), Space: 0B (100%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,8 +1,10 @@
-select  
- case when 
-      id%2 = 1 and id = (select max(id) from Seat) then id
-      when id%2 = 1 then id+1 
-      else id-1
-      end as id, student
-from Seat 
-order by id;
+with cte1 as(
+    select id, case when id%2 = 1 then id+1 else id-1 end as new_id, student
+    from Seat
+),
+ cte as(
+    select  row_number() over (order by new_id asc) as rnk,  student
+from cte1
+)
+select  rnk as id, student
+ from cte; 
