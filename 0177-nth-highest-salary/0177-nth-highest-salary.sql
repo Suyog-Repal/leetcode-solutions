7e96faff9aee45e859038ce674e30cb5e49ecdc4
Suyog Repal <suyog.repal23@spit.ac.in>
2026-09-19 22:28:18 +0530

    Time: 471 ms (70.84%), Space: 0B (100%) - LeetHub

--- 0177-nth-highest-salary/0177-nth-highest-salary.sql
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -1,14 +1,13 @@
-CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
-BEGIN
-set N = N-1;
-  RETURN (
-    with cte as (
-        select distinct salary 
-     from Employee 
+create function getNthHighestSalary(N int) RETURNS INT
+BEGIN 
+SET N = N-1;
+RETURN (
+   WITH cte AS (
+     SELECT  distinct salary
+     from Employee
      order by salary desc
      limit 1 offset N
-    )
-    select salary
-    from cte
-  );
+   )
+   select salary from cte
+);
 END
