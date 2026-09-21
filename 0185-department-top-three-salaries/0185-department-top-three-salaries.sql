cc35804fbc5cea309ef9b9f6fdc9eb168348f6a5
Suyog Repal <suyog.repal23@spit.ac.in>
2026-09-21 20:13:07 +0530

    Time: 1066 ms (65.17%), Space: 0B (100%) - LeetHub

--- 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -1,10 +1,13 @@
 with cte as (
-    select  d.name as Department, e.name as Employee, e.salary as Salary, dense_rank() over (partition by e.departmentId order by e.salary desc) as rnk
+    select e.id, e.name, salary, d.name as department_name
     from Employee e
     left join Department d
     on e.departmentId = d.id
 )
-
-select  Department, Employee, Salary
-from cte
-where rnk <=3
+select Department, Employee, Salary
+from (
+    select department_name as Department, name as Employee, salary as Salary, dense_rank() over (partition by department_name order by salary desc) as rnk
+ from cte
+) as new_table
+where rnk <= 3
+order by Department asc; 
